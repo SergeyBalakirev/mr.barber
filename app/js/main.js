dde57e0e6e0f7637f1454f15688088319de42cbe
SergeyBalakirev <balakirev.sergey87@gmail.com>
2020-06-13 22:54:41 +0300

    added look book

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function () {
 
-
+  $('.look-book__inner').slick({
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
 
 
 
