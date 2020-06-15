b9dbeb9333169fd61114a46076c6c6ec38d9fc97
SergeyBalakirev <balakirev.sergey87@gmail.com>
2020-06-15 22:43:27 +0300

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -9,8 +9,17 @@ $(function () {
     autoplaySpeed: 5000,
   });
 
+  $('.testimonials__slider-inner').slick({
+    prevArrow: '<button class="slick-arrow slick-prev icon-lnr-arrow-left"></button>',
+    nextArrow: '<button class="slick-arrow slick-next icon-lnr-arrow-right"></button>',
+  });
 
-
+  $('.news__slider-inner').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
 
 
 });
